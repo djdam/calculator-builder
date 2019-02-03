d62bcd223964c16be96b7e2188ef420d0ff026dc
Dennis Dam <dennis.dam@gmail.com>
2019-02-03 20:31:57 +0100

    initial commit

---
+++ build/precache-manifest.41a973bbc5728031335cac28e56ed076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56d4b86ac20e03effcb1",
+    "url": "/static/js/main.56d4b86a.chunk.js"
+  },
+  {
+    "revision": "b54b921d47441d2a48b5",
+    "url": "/static/js/1.b54b921d.chunk.js"
+  },
+  {
+    "revision": "56d4b86ac20e03effcb1",
+    "url": "/static/css/main.da1f499e.chunk.css"
+  },
+  {
+    "revision": "0b551ddfe0cc95dc1f4c269dd5041e6f",
+    "url": "/index.html"
+  }
+];
